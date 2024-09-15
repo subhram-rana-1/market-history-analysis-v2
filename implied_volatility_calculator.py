9e23a858cb1fc55e3e2d955a4dcc44a0e8c3c09f
Subhram Subhrajyoti Rana <Subhram@MacBookPro.local>
2024-09-15 16:39:12 +0530

    code formatting

--- implied_volatility_calculator.py
+++ implied_volatility_calculator.py
@@ -4,8 +4,6 @@ from scipy.optimize import minimize_scalar
 import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
-N = norm.cdf
-
 
 N = norm.cdf
 
@@ -15,32 +13,36 @@ N = norm.cdf
 # T : time until option expiration
 # σ: annualized volatility of the asset's returns
 
+
 def BS_CALL(S, K, T, r, sigma):
-d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
-d2 = d1 - sigma * np.sqrt(T)
-return S * N(d1) - K * np.exp(-r*T)* N(d2)
+    d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+
+    return S * N(d1) - K * np.exp(-r*T)* N(d2)
+
 
 def BS_PUT(S, K, T, r, sigma):
-d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
-d2 = d1 - sigma* np.sqrt(T)
-return K*np.exp(-r*T)*N(-d2) - S*N(-d1)
+    d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
+    d2 = d1 - sigma* np.sqrt(T)
+
+    return K*np.exp(-r*T)*N(-d2) - S*N(-d1)
+
 
 def implied_vol(opt_value, S, K, T, r, type_='call'):
-try:
-def call_obj(sigma):
-return abs(BS_CALL(S, K, T, r, sigma) - opt_value)
+    try:
+        def call_obj(sigma):
+            return abs(BS_CALL(S, K, T, r, sigma) - opt_value)
 
-def put_obj(sigma):
-return abs(BS_PUT(S, K, T, r, sigma) - opt_value)
+        def put_obj(sigma):
+            return abs(BS_PUT(S, K, T, r, sigma) - opt_value)
 
-if type_ == 'call':
-res = minimize_scalar(call_obj, bounds=(0.01,6), method='bounded')
-return res.x
-elif type_ == 'put':
-res = minimize_scalar(put_obj, bounds=(0.01,6),
-method='bounded')
-return res.x
-else:
-raise ValueError("type_ must be 'put' or 'call'")
-except:
-return 0
+        if type_ == 'call':
+            res = minimize_scalar(call_obj, bounds=(0.01,6), method='bounded')
+            return res.x
+        elif type_ == 'put':
+            res = minimize_scalar(put_obj, bounds=(0.01,6), method='bounded')
+            return res.x
+        else:
+            raise ValueError("type_ must be 'put' or 'call'")
+    except Exception:
+        raise
